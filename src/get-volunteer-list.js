18ecb7557660f8ad55b6d7061811366094531926
“sangjun121” <juncho12011201@gmail.com>
2023-08-01 01:08:03 +0900

    [ADD]비동기 함수를 이용하여 지역코드를 반환해주는 공공데이터 포털 외부 API연결 및 결과 지역코드 반환 함수 구현

--- src/get-volunteer-list.js
+++ src/get-volunteer-list.js
@@ -1,2 +1,11 @@
-const getVolunteerList = (req, res) => {};
+const getLocationCode = require('./openAPI/get-location-code');
+
+async function getVolunteerList(req, res) {
+    const locationSido = req.body.sido;
+    const locationGugun = req.body.gugun;
+
+    const locationCode = await getLocationCode(locationSido, locationGugun);
+    console.log(locationCode);
+}
+
 module.exports = getVolunteerList;
--- src/openAPI/get-location-code.js
+++ src/openAPI/get-location-code.js
@@ -3,48 +3,68 @@ const xmlToJsonConverter = require('xml-js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const getLocationCode = (req, res) => {
-    let sidoCode, gugunCode;
-    const endPointOfGetAreaCode = process.env.OPENAPI_LOCATION_CODE_END_POINT;
+const locationCode = {}; //텍스트 주소값을 코드로 바꾸고 저장할 객체
 
-    const paramSido = req.body.sido;
-    const paramGugun = req.body.gugun;
+const addValueToObject = (sidoCode, gugunCode) => {
+    //변수 값들 객체에 추가하는 함수
+    locationCode.sidoCode = sidoCode;
+    locationCode.gugunCode = gugunCode;
+};
 
-    if (paramSido === '전체') {
-        //지역 정보 입력 없이 "전체"인 경우
-        sidoCode = null;
-        gugunCode = null;
-        return;
-    }
+function getLocationCode(locationSido, locationGugun) {
+    return new Promise((resolve, reject) => {
+        let sidoCode, gugunCode;
+        const endPointOfGetAreaCode =
+            process.env.OPENAPI_LOCATION_CODE_END_POINT;
 
-    if (gugunCode === '전체') {
-        //구,군 코드가 "전체"인 경우
-        return;
-    }
-
-    const requestOptions = {
-        url: endPointOfGetAreaCode,
-        qs: {
-            schSido: paramSido,
-            schGugun: paramGugun,
-        },
-    };
-
-    request(requestOptions, function (error, response, body) {
-        if (error) {
-            console.log(error);
+        if (locationSido === '전체') {
+            //지역 정보 입력 없이 "전체"인 경우
+            sidoCode = null;
+            gugunCode = null;
+            addValueToObject(sidoCode, gugunCode);
             return;
         }
 
-        if (response.statusCode == 200) {
-            let resultByXML = body;
-            let resultByJSON = xmlToJsonConverter.xml2json(resultByXML, {
-                compact: true,
-                spaces: 4,
-            });
-            console.log(resultByJSON);
+        if (locationGugun === '전체') {
+            //구,군 코드가 "전체"인 경우
+
+            return;
         }
+
+        //시도, 구군 정보가 둘다 들어왔을 경우 (둘다 전체가 아닐 경우)
+        const requestOptions = {
+            url: endPointOfGetAreaCode,
+            qs: {
+                schSido: locationSido,
+                schGugun: locationGugun,
+            },
+        };
+
+        request(requestOptions, function (error, response, body) {
+            if (error) {
+                console.log(error);
+                reject(error);
+                return;
+            }
+
+            if (response.statusCode == 200) {
+                let resultByXML = body;
+                let resultByJSON = xmlToJsonConverter.xml2json(resultByXML, {
+                    compact: true,
+                    spaces: 4,
+                });
+
+                let resultByObject = JSON.parse(resultByJSON); //json형식의 결과 값을 객체로 바꾸기
+
+                sidoCode = resultByObject.response.body.items.item.sidoCd._text; //해당 데이터에 접근
+                gugunCode =
+                    resultByObject.response.body.items.item.gugunCd._text;
+
+                addValueToObject(sidoCode, gugunCode);
+                resolve(locationCode);
+            }
+        });
     });
-};
+}
 
 module.exports = getLocationCode;
