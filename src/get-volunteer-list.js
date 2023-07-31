5b092deb48c05a4052a3948ad6866b4861ee7ba4
“sangjun121” <juncho12011201@gmail.com>
2023-08-01 02:05:18 +0900

    [ADD] 공공데이터 포털 API를 이용하여 봉사활동 리스트 응답하는 API 개발 완료

--- src/get-volunteer-list.js
+++ src/get-volunteer-list.js
@@ -1,10 +1,14 @@
 const getLocationCode = require('./openAPI/get-location-code-by-openAPI');
+const getVolunteerInfo = require('./openAPI/get-volunteer-info-by-openAPI');
 
 async function getVolunteerList(req, res) {
     const locationSido = req.body.sido;
     const locationGugun = req.body.gugun;
 
     const locationCode = await getLocationCode(locationSido, locationGugun);
+    const volunteerList = await getVolunteerInfo(locationCode);
+
+    res.json({ success: true, data: volunteerList });
 }
 
 module.exports = getVolunteerList;
