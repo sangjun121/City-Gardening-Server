6139d771adfd7f1c314dd0b935747935dfca2630
“sangjun121” <juncho12011201@gmail.com>
2023-08-01 01:15:22 +0900

    [FIX] 파일명 수정

--- src/get-volunteer-list.js
+++ src/get-volunteer-list.js
@@ -1,11 +1,10 @@
-const getLocationCode = require('./openAPI/get-location-code');
+const getLocationCode = require('./openAPI/get-location-code-by-openAPI');
 
 async function getVolunteerList(req, res) {
     const locationSido = req.body.sido;
     const locationGugun = req.body.gugun;
 
     const locationCode = await getLocationCode(locationSido, locationGugun);
-    console.log(locationCode);
 }
 
 module.exports = getVolunteerList;
