7bf8bf5f8d419622c5f4522e203c21ddf51120f2
“sangjun121” <juncho12011201@gmail.com>
2023-08-02 00:20:57 +0900

    [FIX] 회원가입시 아이디 중복 처리완료

--- src/add-user.js
+++ src/add-user.js
@@ -1,9 +1,6 @@
 const dbPool = require('../config/database-pool.js');
 
 const adduser = (req, res) => {
-    //"/process/adduser"는 수신자. 이 주소로 post형태로 요청하면 뒤의 함수가 실행된다.
-    console.log('/process/adduser가 호출됨 ' + req);
-
     const paramId = req.body.id; //위에 use(json과 urlencoded)덕분에 알맞은 형태로 변경해주기 때문에 req.body 식으로 접근가능하다
     const paramName = req.body.name;
     const paramAge = req.body.age;
@@ -18,8 +15,6 @@ const adduser = (req, res) => {
             return;
         }
 
-        console.log('데이터베이스 연결 끈 얻었음');
-
         const exec = conn.query(
             'insert into users_information (id, name, age, password) values (?,?,?,?);',
             [paramId, paramName, paramAge, paramPassword],
@@ -28,22 +23,27 @@ const adduser = (req, res) => {
                 console.log('실행된 sql: ' + exec.sql); //exec.sql하면 우리가 실제로 보냈던 쿼리문이 ?에 값이 치환된걸로 보여준다.
 
                 if (err) {
-                    console.log('쿼리문 실행시 오류 발생');
-                    console.dir(err); //디렉토리 형식으로 오류 출력 (계층구조로 출력한다, 객체의 속성을 출력한다, 요소를 json과 같은 트리구조로 출력한다.)
-
-                    res.end();
+                    //쿼리문 실행시 오류 발생
+                    if (err.code === 'ER_DUP_ENTRY') {
+                        //아이디 중복일 경우 쿼리문 실행 실패
+                        res.json({
+                            success: false,
+                            reason: 'Duplicate_Entry_for_ID',
+                        });
+                        return;
+                    }
+                    res.json({ success: false }); //나머지 이유로 쿼리문 실행 실패시
                     return;
                 }
 
-                if (result) {
-                    console.dir(result);
-                    console.log('insert 성공');
-
+                if (result.length > 0) {
+                    console.log('회원가입 성공');
                     res.json({ success: true });
-                } else {
-                    console.log('insert 실패');
-                    res.json({ success: false });
+                    return;
                 }
+
+                console.log('회원가입 실패');
+                res.json({ success: false });
             }
         );
     });
