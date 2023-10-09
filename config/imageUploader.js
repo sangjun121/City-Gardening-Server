010ac742023e479ee0d795fae29e5cdb56703d8a
“sangjun121” <juncho12011201@gmail.com>
2023-10-09 16:49:38 +0900

    [ADD] 이미지 저장 aws s3연결 코드 작성

--- config/imageUploader.js
+++ config/imageUploader.js
@@ -1,14 +1,27 @@
-const fs = require('fs');
-const AWS = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const { S3Client, ListBucketsCommand } = require('@aws-sdk/client-s3');
+const s3Client = new S3Client({ region: 'ap-northeast-2' });
 const dotenv = require('dotenv');
 dotenv.config();
 
-const s3 = new AWS.S3({
+const s3 = new S3Client({
+    //aws 구성정보 저장
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_S3_REGION,
 });
 
-const params = {
-    Bucket: process.env.BUCKET_NAME,
-    Key: process.env.BUCKET_KEY + 'section1',
-};
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.BUCKET_NAME,
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req, file, cb) {
+            cb(null, `${Date.now()}_${file.originalname}`);
+        },
+    }),
+});
+
+module.exports = upload;
--- src/routes/land.js
+++ src/routes/land.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const connectToDatabase = require('../../config/database-pool');
-
-const upload = multer({
-    dest: 'upload/',
-});
+const upload = require('../../config/imageUploader');
 
 router.post('/register', upload.single('images'), async (req, res) => {
     //오염지역 등록한 사용자이름도 추가
@@ -15,18 +12,31 @@ router.post('/register', upload.single('images'), async (req, res) => {
     const paramLongitude = req.body.longitude;
     const paramPollutionProgress = req.body.pollutionProgress;
 
+    //1. 클라이언트에서 보낸 이미지 데이터 저장
     const landData = {
         detailDescription: paramDetailDescription,
         latitude: paramLatitude,
         longitude: paramLongitude,
         pollutionProgress: paramPollutionProgress,
     };
+
     console.log(req.file);
-    if (req.file) {
-        res.json({ success: true });
-    } else {
-        res.json({ success: false });
-    }
+
+    // const db = await connectToDatabase();
+    // db.collection('polluctionLandInformation').insertOne(landData);
+
+    // try {
+    //     db.collection('polluctionLandInformation').insertOne(landData);
+    //     console.log('저장 완료');
+    //     res.json({ success: true, description: '오염구역 등록 완료' });
+    //     //커낵션 닫을 필요 없음 몽고디비 드라이버가 내부적으로 커넥션 풀링을 관리하고 insertone함수 호출 후 자동으로 커넥션을 반환하고 닫음
+    // } catch (error) {
+    //     console.error('Error inserting document:', error);
+    //     res.json({
+    //         success: false,
+    //         error: `오염구역 데이터 베이스 저장 실패, 에러코드: ${error}`,
+    //     });
+    // }
 });
 
 module.exports = router;
