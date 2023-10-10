0c512b559169ed3e72c1cf6896d27a4f0a85109c
“sangjun121” <juncho12011201@gmail.com>
2023-10-10 17:42:10 +0900

    [FIX] 이미지 데이터 등록 응답 코드 설정

--- src/routes/land.js
+++ src/routes/land.js
@@ -22,21 +22,29 @@ router.post('/register', upload.single('images'), async (req, res) => {
     console.log(req.file);
     console.log(req.files);
 
-    const db = await connectToDatabase();
-    db.collection('polluctionLandInformation').insertOne(landData);
-
-    try {
-        db.collection('polluctionLandInformation').insertOne(landData);
-        console.log('저장 완료');
-        res.json({ success: true, description: '오염구역 등록 완료' });
-        //커낵션 닫을 필요 없음 몽고디비 드라이버가 내부적으로 커넥션 풀링을 관리하고 insertone함수 호출 후 자동으로 커넥션을 반환하고 닫음
-    } catch (error) {
-        console.error('Error inserting document:', error);
-        res.json({
-            success: false,
-            error: `오염구역 데이터 베이스 저장 실패, 에러코드: ${error}`,
-        });
+    if (req.file) {
+        res.json({ success: 'true' });
+    } else if (req.files) {
+        res.json({ success: 'trues' });
+    } else {
+        res.json({ success: 'false' });
     }
+
+    // const db = await connectToDatabase();
+    // db.collection('polluctionLandInformation').insertOne(landData);
+
+    // try {
+    //     db.collection('polluctionLandInformation').insertOne(landData);
+    //     console.log('저장 완료');
+    //     res.json({ success: true, description: '오염구역 등록 완료' });
+    //     //커낵션 닫을 필요 없음 몽고디비 드라이버가 내부적으로 커넥션 풀링을 관리하고 insertone함수 호출 후 자동으로 커넥션을 반환하고 닫음
+    // } catch (error) {
+    //     console.error('Error inserting document:', error);
+    //     res.json({
+    //         success: false,
+    //         error: `오염구역 데이터 베이스 저장 실패, 에러코드: ${error}`,
+    //     });
+    // }
 });
 
 const prjection = {};
